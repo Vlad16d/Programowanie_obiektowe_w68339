bd150c566468da081c0d81f976c1144a078d5cb0
Uladzislau Bainiarovich <118025624+Vlad16d@users.noreply.github.com>
2024-10-22 16:51:22 +0200

    Add files via upload

---
+++ Lab2/Lab2/Car.cs
@@ -0,0 +1,39 @@
+﻿
+using SharpLab;
+
+//public class Car
+//{
+//    // Pole prywatne
+//    private string marka;
+//    // Właściwość z get i set
+//    public string Marka
+//    {
+//        get
+//        {
+//            return marka; // Zwraca wartość pola 'marka'
+//        }
+//        set
+//        {
+//            marka = value; // Ustawia wartość pola 'marka'
+//        }
+//    }
+//}
+
+
+
+
+// --- Main ---
+
+
+try
+{
+    Person person = new Person("jan", "nowak", 12);
+    person.View();
+}
+
+catch(ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+// ------
--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+using System;
+using System.Threading.Channels;
+using System.Collections.Concurrent;
+using System.Numerics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data;
+using System;
+
+
+namespace SharpLab
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    //Console.WriteLine($"Imie musi miec co najmniej 2 znaki");
+                    throw new ArgumentException($"Imie musi miec co najmniej 2 znaki");
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentException($"Nazwisko musi miec co najmniej 2 znaki");
+                }
+                lastName = value;
+            }
+        }
+
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (age <= 0) throw new ArgumentException($"wiek musi byc liczba dodatnia");
+            }
+        }
+
+        public void View()
+        {
+            Console.WriteLine($"Imie: {firstName} \tNazwisko {lastName} \twiek {age}");
+        }
+    }
+}
