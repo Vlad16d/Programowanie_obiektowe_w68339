25cb70c6ac52aae176a8eacca593c5138fc7b752
Uladzislau Bainiarovich <118025624+Vlad16d@users.noreply.github.com>
2024-12-01 17:00:49 +0100

    Add files via upload

---
+++ Lab1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+using System.Threading.Channels;
+
+namespace project
+{
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Robot bot = new Robot("Bot", 800, new byte[] { 0, 0, 0 });
+            bot.printValues();
+
+
+            Robot killer = new Robot();
+            killer.setValues("Killer", 1000, new byte[] { 0, 0, 10 });
+            killer.printValues();
+
+            //Robot.count = 5;
+            Robot.Print();
+
+        }
+    }
+}
---
+++ Lab1/ConsoleApp1/Robot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Robot
+    {
+
+        public static int count = 0;
+
+        private string name;
+        private int weight;
+        private byte[] coordinates;
+
+        public Robot(string _name, int _weight, byte[] _coordinates) 
+        {
+            Console.WriteLine("Object has been created ");
+            setValues(_name, _weight, _coordinates);
+            count++;
+        }
+
+        public Robot() {}
+
+        public void setValues(string _name, int _weight, byte[] _coordinates)
+        {
+            name = _name;
+            weight = _weight;
+            coordinates = _coordinates;
+            count++;
+        }
+
+        public void printValues()
+        {
+            Console.WriteLine(name + " weight " + weight + ".Coordinates: ");
+            foreach(byte el in coordinates)
+                Console.WriteLine(el);
+        }
+
+        public static void Print() 
+        { 
+            Console.WriteLine("\nCount is " + count);
+        }
+
+    }
+}
